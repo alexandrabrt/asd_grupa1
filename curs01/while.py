e50d7dc304f97150305f470fe967a85280b0d403
Alexandra Bratu <alexandra.bratu@nxdata.ro>
2023-07-27 21:19:07 +0300

    curs1

---
+++ curs01/while.py
@@ -0,0 +1,25 @@
+# while True:
+#     print("Set instructiuni")
+#     break
+
+# a = 2
+# while a > 1:
+#     a = a - 1 # a -= 1
+#     print(a)
+# print(list(range(2, 10, 3)))
+# for i in range(2, 10, 3):
+#     print(f"Set instructiuni {i}")
+
+# lista = [45, 62, 73]
+# for index, value in enumerate(lista):
+#     print(index, value)
+# dict_1 = {"key_1": 45,
+#           "key_2": 62,
+#           "key_3": 73}
+# for i in dict_1:
+# for i in dict_1.keys():
+# for i in dict_1.values():
+# for i, v in dict_1.items():
+#     print(i, v)
+
+# a, b, c = 1, 2, 3
