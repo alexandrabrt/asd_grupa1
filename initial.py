81515a06322733525cc51deb35210295630b7141
Alexandra Bratu <alexandra.bratu@nxdata.ro>
2023-09-28 23:43:05 +0300

    pandas

--- initial.py
+++ initial.py
@@ -1,1 +1,39 @@
-print("Al doilea mesaj")
+# print("Al doilea mesaj")
+
+
+# def functie1(n):
+#     # global n
+#     # n = 2
+#     # global n
+#     if n % 2 == 0:
+#         print(True)
+
+
+
+
+# print(functie1(3))
+# print(n)
+
+# data = {'1': 2, 1: 3, '5': [1, 2]}
+# for i, v in data.items():
+#     if i == 1:
+#         break
+#     print(v)
+
+# my_list = [2, 1, 5]
+# try:
+#     msg = 4
+#     # global msg
+#     x = my_list[3]
+# except Exception:
+#     print(x)
+#
+# var_set = (1,)
+# print(type(var_set))
+
+test, test1 = 'Ana are mere', 6
+for i, v in enumerate(test):
+    if test1 == 6:
+        test1 = 3
+suma = test + test1
+print(suma)
